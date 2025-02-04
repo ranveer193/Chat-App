1be81127534503b8bba4a00ed6aa88970953e0e2
ranveer193 <ranveerdhindsa921@gmail.com>
2025-02-04 18:33:49 +0530

    new commit

--- frontend/src/redux/authReducer/action.js
+++ frontend/src/redux/authReducer/action.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as types from "./actionType";
 
-const END_POINT="https://chatc.onrender.com"
+const END_POINT="https://chat-app-eta-sooty.vercel.app"
 
 //  sign up user / create account
 const createAccount = (user) => async (dispatch) => {
