3f2f1d2c19d055239e87eb80c9f3a4dabf577b28
ranveer193 <ranveerdhindsa921@gmail.com>
2025-02-04 18:16:58 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,62 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectionDB = require("./config/db");
-const userRoutes = require("./routes/user.routes")
-const chatRoutes = require("./routes/chat.routes")
-const messageRoutes = require("./routes/message.routes")
-
+const userRoutes = require("./routes/user.routes");
+const chatRoutes = require("./routes/chat.routes");
+const messageRoutes = require("./routes/message.routes");
 const cors = require('cors');
 
 const app = express();
 dotenv.config();
 connectionDB();
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    console.log("welcome to chat app");
-})
+    console.log("Welcome to Chat App");
+    res.send("Welcome to Chat App");
+});
 
-// app.use("/api/user", userRoutes);
-// app.use("/api/chat", chatRoutes);
-// app.use("/api/message", messageRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
-const port = process.env.PORT || 8001
-const server = app.listen(port, console.log("server is running at post = ", port));
+const port = process.env.PORT || 8001;
+const server = app.listen(port, () => console.log("Server is running at port:", port));
 
-// const io = require("socket.io")(server, {
-//     cors: {
-//         origin: "https://chat-app-ca.netlify.app", 
-//     },
-// });
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "https://chat-app-a33u.vercel.app",
+    },
+});
 
-// io.on("connection", (socket) => {
-//     socket.on("setup", (userData) => {
-//         socket.join(userData._id);
-//         socket.emit("connected")
-//     })
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-//     socket.on("join chat", (room) => {
-//         socket.join(room)
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connected");
+    });
 
-//     })
+    socket.on("join chat", (room) => {
+        socket.join(room);
+    });
 
-//     socket.on("new message", (newMessageRec) => {
-//         var chat = newMessageRec.chat;
-//         if (!chat.users) return console.log("chat user not defined");
+    socket.on("new message", (newMessageRec) => {
+        let chat = newMessageRec.chat;
+        if (!chat || !chat.users) {
+            console.log("Chat users not defined");
+            return;
+        }
 
-//         chat.users.forEach(user => {
-//             if (user != newMessageRec.sender._id) {
-//                 socket.in(user).emit("message received", newMessageRec);
-//             }
-//         });
-//     });
+        chat.users.forEach(user => {
+            if (user !== newMessageRec.sender._id) {
+                socket.in(user).emit("message received", newMessageRec);
+            }
+        });
+    });
 
-
-
-// });
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
